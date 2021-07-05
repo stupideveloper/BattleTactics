aa2e1d9e229c70c1d10b9d51380aab6a4701795e
Bloxigus <Bloxigus@outlook.com>
2021-07-05 18:02:05 +0930

    update

--- src/main/java/dev/tenfov/battletactics/MobsBasic.java
+++ src/main/java/dev/tenfov/battletactics/MobsBasic.java
@@ -6,21 +6,24 @@ import dev.tenfov.battletactics.events.TabCompleteSpawn;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MobsBasic extends JavaPlugin {
+public final class MobsBasic extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         this.getCommand("mobspawn").setExecutor(new Spawn());
         this.getCommand("mobspawn").setTabCompleter(new TabCompleteSpawn());
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
     @EventHandler
     public void onMobDamage(EntityDamageEvent e) {
