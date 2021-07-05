708439e962c1e8cbe51792b4cd593e85ee2f0a0d
Bloxigus <Bloxigus@outlook.com>
2021-07-05 19:09:19 +0930

    fix 2.0

--- src/main/java/dev/tenfov/battletactics/mobs/healthbar/HealthBarEntity.java
+++ src/main/java/dev/tenfov/battletactics/mobs/healthbar/HealthBarEntity.java
@@ -3,16 +3,15 @@ package dev.tenfov.battletactics.mobs.healthbar;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
 
-import net.minecraft.world.entity.AreaEffectCloud;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.decoration.ArmorStand;
 
-public class HealthBarEntity extends AreaEffectCloud {
+public class HealthBarEntity extends ArmorStand {
 
     public HealthBarEntity(Location loc) {
-        super(EntityType.AREA_EFFECT_CLOUD, ((CraftWorld) loc.getWorld()).getHandle());
+        super(EntityType.ARMOR_STAND, ((CraftWorld) loc.getWorld()).getHandle());
         this.setPos(loc.getX(), loc.getY(), loc.getZ());
         this.setInvulnerable(true);
-        this.setDuration(2147483647);
     }
     
 }
