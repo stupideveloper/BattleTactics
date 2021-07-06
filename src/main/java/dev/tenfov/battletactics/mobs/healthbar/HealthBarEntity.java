1c6ddb8d72504f40b3302e2e624cad20c8526169
Bloxigus <Bloxigus@outlook.com>
2021-07-06 13:39:09 +0930

    Update HealthBarEntity.java

--- src/main/java/dev/tenfov/battletactics/mobs/healthbar/HealthBarEntity.java
+++ src/main/java/dev/tenfov/battletactics/mobs/healthbar/HealthBarEntity.java
@@ -20,20 +20,35 @@ public class HealthBarEntity extends ArmorStand {
     public HealthBarEntity(Location loc, Entity parent) {
         super(EntityType.ARMOR_STAND, ((CraftWorld) loc.getWorld()).getHandle());
         this.setPos(loc.getX(), loc.getY(), loc.getZ());
+        
+        // Makes the ArmorStand unkillable
         this.setInvulnerable(true);
+
+        // Makes the ArmorStand invisible
         this.setInvisible(true);
+
+        // Sets the parent entity
         this.parent = parent;
         if (parent instanceof LivingEntity) {
+            
+            // Makes getting the health of the parent much easier
             this.parentEntity = (LivingEntity) parent;
         } else {
+            // If the parent isnt a LivingEntity, remove the ArmorStand
             this.remove(RemovalReason.DISCARDED);
         }
     }
     @Override 
     public void tick() {
         try {
+
+            // Sets the location to that of the parent entity
             this.setPos(parent.getX(), parent.getY() + 0.25, parent.getZ());
+
+            // Calculates the health of the parent entity
             Float hea = (this.parentEntity.getHealth() + this.parentEntity.getAbsorptionAmount());
+
+            // Gives the ArmorStand a name
             this.setCustomName(new TextComponent(hea.shortValue() + " ❤"));
             this.setCustomNameVisible(true);
             this.errors = 0;
@@ -43,6 +58,8 @@ public class HealthBarEntity extends ArmorStand {
                 this.remove(RemovalReason.KILLED);
             }
         } catch (NullPointerException e) {
+
+            // Remove if the parent entity never spawns
             this.errors = this.errors + 1;
             if (this.errors > 40) {
                 this.remove(RemovalReason.KILLED);
