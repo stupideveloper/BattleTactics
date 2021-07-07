93dd71093b58ae5652da4a4f132e575814ad674f
Lachlan Kemp <me@lachlankemp.com>
2021-07-07 16:43:49 +0930

    changed to adventure mode on join

--- src/main/java/dev/tenfov/battletactics/events/JoinEvent.java
+++ src/main/java/dev/tenfov/battletactics/events/JoinEvent.java
@@ -1,5 +1,6 @@
 package dev.tenfov.battletactics.events;
 
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -7,6 +8,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinEvent implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        event.getPlayer().setResourcePack("https://www.dropbox.com/s/hpd705war283fhx/Jicklus%2B1.17.zip?dl=1"); // Direct Download to texture pack..
+        //event.getPlayer().setResourcePack("https://www.dropbox.com/s/hpd705war283fhx/Jicklus%2B1.17.zip?dl=1"); // Direct Download to texture pack..
+        event.getPlayer().setGameMode(GameMode.ADVENTURE);
     }
 }
